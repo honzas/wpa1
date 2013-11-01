2f7f852946a8219d9f7f5badb9b097f4720462c7
Jan Stepanek <jan.stepanek@nodus.cz>
2013-11-01 16:16:39 +0100

    new listener added

---
+++ src/main/java/cz/cvut/stepaj29/listener/DummyListener.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.stepaj29.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author jan
+ */
+public class DummyListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
